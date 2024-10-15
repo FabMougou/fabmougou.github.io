0ea246da855b5a4710a88fffe7fbede570b9b866
FabMougou <fabricio.mougou@gmail.com>
2024-10-15 13:52:53 +0100

    implemented difficulty slider

--- learn_french/index.js
+++ learn_french/index.js
@@ -1,12 +1,17 @@
 let words = [];
+let currentLanguage = 'french';
+
 let otherBox = document.getElementById('other-word');
 let englishBox = document.getElementById('english-word');
-let currentLanguage = 'french';
 let otherWord = '';
 let englishWord = '';
 
+let difficultySlider = document.getElementById('difficulty-slider');
+let difficultyAmount = document.getElementById('difficulty-amount');
+let difficulty = difficultySlider.value;
+
 function loadWords() {
-    fetch(`common_${currentLanguage}_words.csv`) 
+    fetch(`word_lists/common_${currentLanguage}_words.csv`) 
     .then(response => response.text())
     .then(data => {
         Papa.parse(data, {
@@ -26,7 +31,7 @@ function delay(time) {
 
 
 function getRandomWord() {
-    let randomIndex = Math.floor(Math.random() * words.length);
+    let randomIndex = Math.floor(Math.random() * difficulty);
     otherWord = words[randomIndex][1];
     console.log(words[randomIndex][0]);
     otherBox.value = otherWord;
@@ -63,6 +68,23 @@ function changeLanguage(language) {
     document.getElementById('current-language').innerText = language.charAt(0).toUpperCase() + language.slice(1);
     document.getElementById('other-flag').src = `images/${language}_flag.svg`;
     loadWords();
+}
+
+function changeDifficulty() {
+    difficulty = difficultySlider.value;
+    difficultyAmount.innerText = difficulty;
+    
+    const blue = [0, 0, 255]; 
+    const red = [255, 0, 0]; 
+    const factor = (difficulty - 100) / (1000 - 100);
+
+    const r = Math.round(blue[0] + factor * (red[0] - blue[0]));
+    const g = Math.round(blue[1] + factor * (red[1] - blue[1]));
+    const b = Math.round(blue[2] + factor * (red[2] - blue[2]));
+    console.log(r, g, b);
+
+    difficultyAmount.style.color = `rgb(${r}, ${g}, ${b})`;
+
 }
 
 changeLanguage('french');
