6700416bf80370f4062d7a66b678cab22066da80
FabMougou <fabricio.mougou@gmail.com>
2026-04-16 17:37:22 +0100

    cleaned up

--- portfolio/src/components/index.jsx
+++
@@ -1,5 +0,0 @@
-export { default as ASCIIText } from './ASCIIText.jsx';
-export { default as ASCIIWave } from './ASCIIWave.jsx';
-export { default as LoadingSplash } from './LoadingSplash.jsx'
-export { default as DecryptedText } from './DecryptedText.jsx'
-export { default as CardDeck } from './CardDeck.jsx'
